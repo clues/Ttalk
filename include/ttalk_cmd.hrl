66be67084e7c22f4804ae9ac569bb3cf6356eb59
chao <lino.chao@gmail.com>
2012-04-10 22:54:13 +0800

    up

--- include/ttalk_cmd.hrl
+++ include/ttalk_cmd.hrl
@@ -1,5 +1,7 @@
 
--record(line,{cmd='undefined',data=[]}).
+-record(line,{cmd='undefined',data=[],state=0}).
 
 -define(CMD_HELP,"help").%%-help
 -define(CMD_LOGIN,"login").%%-login
+
+-define(CODE_CMD_FORMAT_ERROR,1).
--- src/ttalk_request.erl
+++ src/ttalk_request.erl
@@ -16,15 +16,16 @@ removeHeadSpace([H|T]) ->
 	end.
 
 parseCmd([],Cmd,Data) ->
-	#line{cmd=lists:reverse(Cmd),data=Data};
+	Line = #line{cmd=lists:reverse(Cmd),data=Data},
+	fliter_cmd(Line);
 
 parseCmd(Line,Cmd,Data) ->
 	[H|T] = Line,
 	if
 		H =/= $\s ->
-			parseLine(T,[H|Cmd],Data);
+			parseCmd(T,[H|Cmd],Data);
 		true ->
-			parseCmd([],Cmd,T)
+			parseCmd([],Cmd,Line)
 	end.
 
 parseLine([],Cmd,Data) ->
@@ -41,6 +42,69 @@ parseLine(Line,Cmd,Data) ->
 			#line{data=Line}
 	end.
 
+get_cmd(Cmd) ->
+	if 
+		Cmd =:= 'undefined' ->
+			'undefined';
+		Cmd =:= ?CMD_HELP ->
+			{ok,?CMD_HELP};
+		Cmd =:= ?CMD_LOGIN ->
+			{ok,?CMD_LOGIN};
+		true ->
+			{none,Cmd}
+	end.
+
+
+fliter_cmd(Line) ->
+	Cmd = get_cmd(Line#line.cmd),
+	case Cmd of
+		'undefined' ->
+			#line{cmd='undefined',data=Line#line.data};
+		{ok,C} ->
+			NewLine = #line{cmd=C,data=Line#line.data},
+			check_cmd_format(NewLine);
+		_ ->
+			#line{cmd='undefined',data=("-"++Line#line.cmd++Line#line.data)}
+	end.
+
+check_cmd_format(Line) ->
+	if
+		Line#line.cmd =:= 'undefined' ->
+			Line;
+		true ->
+			if
+				Line#line.cmd =:= ?CMD_HELP ->
+					format_0(Line);
+				Line#line.cmd =:= ?CMD_LOGIN ->
+					format_1(Line)
+			end
+	end.
+
+format_0(Line) ->
+	if
+		length(Line#line.data) =/= 0 ->
+			#line{cmd=Line#line.cmd,data=Line#line.data,state=?CODE_CMD_FORMAT_ERROR};
+		true ->
+			Line
+	end.
+
+format_1(Line) ->
+	Tokens = string:tokens(Line," "),
+	if
+		length(Tokens) =/= 1 ->
+			#line{cmd=Line#line.cmd,data=Line#line.data,state=?CODE_CMD_FORMAT_ERROR};
+		true ->
+			#line{cmd=Line#line.cmd,data=Tokens}
+	end.
+
+format_2(Line) ->
+	Tokens = string:tokens(Line#line.data," "),
+	if
+		length(Tokens) =/= 2 ->
+			#line{cmd=Line#line.cmd,data=Line#line.data,state=?CODE_CMD_FORMAT_ERROR};
+		true ->
+			#line{cmd=Line#line.cmd,data=Tokens}
+	end.
 
 %%
 %% Tests
@@ -62,4 +126,13 @@ parseLine_without_cmd_test() ->
 
 parseLine_with_cmd_test() ->
 	?assertEqual(#line{cmd="login",data="   jias"},parseLine(" -login   jias",[],[])).
+
+fliter_cmd_with_cmd_test() ->
+	?assertEqual(#line{cmd="login",data="   jias"},parseLine(" -login   jias",[],[])).
+fliter_cmd_with_undefined_cmd_test() ->
+	?assertEqual(#line{cmd='undefined',data="-loginlogin   jias"},parseLine(" -loginlogin   jias",[],[])).
+fliter_cmd_with_cmd_flag_test() ->
+	?assertEqual(#line{cmd='undefined',data="- "},parseLine(" - ",[],[])).
+format_cmd_ok_test() ->
+	?assertEqual(#line{cmd='login',data="jias"},parseLine("-login jias",[],[])).
 -endif.
